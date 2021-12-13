55c869c712c70dbd22395cc28e9ae5ab48c03802
Atharva Hatolkar <atharvahatolkar@gmail.com>
2021-12-13 15:17:52 +0530

    center

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -3,7 +3,7 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'cluster-map',
     style: 'mapbox://styles/mapbox/light-v10',
-    center: [-103.5917, 40.6699],
+    center: [21.879701064440265, 79.0251460360711],
     zoom: 3
 });
 
